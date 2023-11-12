b8e9e12e9e44c0fe79bc438b1332f470ad6943f8
Palash Nath <palashnath880@gmail.com>
2023-11-12 12:39:59 +0600

    Home Hero Slider Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
+import Hero from "@/components/Home/Hero";
 import Head from "next/head";
 
 export default function Home() {
-  return <div>
+  return <>
     <Head>
       <title>Medi Care - Home</title>
     </Head>
 
-    <h2 className="font-bold">Hello world</h2>
+    <Hero />
 
-  </div>
+  </>
 }
