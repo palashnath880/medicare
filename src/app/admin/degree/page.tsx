f6a82738a07b0c8108da375fdbcd1d903e7487e4
Palash Nath <palashnath880@gmail.com>
2024-12-17 14:49:18 +0600

    admin degree page added

--- src/app/admin/degree/page.tsx
+++ src/app/admin/degree/page.tsx
@@ -1,12 +1,231 @@
 "use client";
 
 import PageHeading from "@/components/Admin/shared/PageHeading";
-import React from "react";
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Spinner,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  useDisclosure,
+} from "@nextui-org/react";
+import { Degree } from "@prisma/client";
+import axios, { AxiosError } from "axios";
+import React, { useState } from "react";
+import { useMutation, useQuery } from "react-query";
+import moment from "moment";
+import { FaPlus, FaSearch } from "react-icons/fa";
+import { FiRefreshCw } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
+import { toast } from "react-toastify";
+
+const AddModal = ({
+  isOpen,
+  onOpenChange,
+  refetch,
+}: {
+  isOpen: any;
+  onOpenChange: () => void;
+  refetch: () => void;
+}) => {
+  const [val, setVal] = useState<string>("");
+
+  // add degree
+  const addDegree = useMutation<any, AxiosError<{ message: string }>, string>({
+    mutationFn: (name) => {
+      return axios.post(`/api/degree`, { name });
+    },
+    onSuccess: (_, name) => {
+      toast.success(`${name} added successfully`);
+      setVal("");
+      refetch();
+    },
+  });
+
+  return (
+    <Modal onOpenChange={onOpenChange} isOpen={isOpen} hideCloseButton>
+      <ModalContent>
+        <ModalHeader>Add New Degree</ModalHeader>
+        <ModalBody>
+          <div className="flex flex-col gap-2">
+            <Input
+              type="text"
+              label="Degree Name"
+              color="primary"
+              value={val}
+              onChange={(e) => setVal(e.target.value)}
+              isRequired
+            />
+            {addDegree.isError && (
+              <p className="text-center text-sm text-red-500">
+                {addDegree.error.response?.data?.message ||
+                  "Sorry! Something went wrong"}
+              </p>
+            )}
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="primary"
+            radius="sm"
+            startContent={<FaPlus className="w-3 h-3" />}
+            isDisabled={!val || addDegree.isLoading}
+            onPress={() => addDegree.mutate(val)}
+          >
+            Add Degree
+          </Button>
+          <Button
+            color="danger"
+            radius="sm"
+            startContent={<IoMdClose className="w-4 h-4" />}
+            onPress={onOpenChange}
+          >
+            Dismiss
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
 
 export default function Page() {
+  // modal controller
+  const { isOpen, onOpen, onOpenChange } = useDisclosure({ id: "DegreeAdd" });
+
+  // react-query
+  const {
+    data: degrees,
+    isLoading,
+    isSuccess,
+    refetch,
+  } = useQuery<Degree[]>({
+    queryKey: ["degree"],
+    queryFn: async () => {
+      const res = await axios.get(`/api/degree`);
+      return res.data;
+    },
+  });
+
+  // table top content
+  const topContent = React.useMemo(() => {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between gap-3 items-end">
+          <Input
+            isClearable
+            className="w-full sm:max-w-[30%]"
+            placeholder="Search by name..."
+            startContent={<FaSearch />}
+            // value={filterValue}
+            // onClear={() => onClear()}
+            // onValueChange={onSearchChange}
+          />
+          <div className="flex gap-3">
+            {/* <Dropdown>
+              <DropdownTrigger className="hidden sm:flex">
+                <Button
+                  endContent={<ChevronDownIcon className="text-small" />}
+                  variant="flat"
+                >
+                  Columns
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label="Table Columns"
+                closeOnSelect={false}
+                selectedKeys={visibleColumns}
+                selectionMode="multiple"
+                onSelectionChange={setVisibleColumns}
+              >
+                {columns.map((column) => (
+                  <DropdownItem key={column.uid} className="capitalize">
+                    {capitalize(column.name)}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown> */}
+            <Button
+              color="primary"
+              startContent={<FiRefreshCw />}
+              onPress={() => refetch()}
+            >
+              Refresh
+            </Button>
+            <Button color="primary" startContent={<FaPlus />} onPress={onOpen}>
+              Add New
+            </Button>
+          </div>
+        </div>
+        {/* <div className="flex justify-between items-center">
+          <span className="text-default-400 text-small">
+            Total {users.length} users
+          </span>
+          <label className="flex items-center text-default-400 text-small">
+            Rows per page:
+            <select
+              className="bg-transparent outline-none text-default-400 text-small"
+              onChange={onRowsPerPageChange}
+            >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+          </label>
+        </div> */}
+      </div>
+    );
+  }, [refetch, onOpen]);
+
   return (
     <>
       <PageHeading>Degree</PageHeading>
+
+      <Table
+        aria-label="Degree Table"
+        classNames={{ base: "!mt-10" }}
+        topContent={topContent}
+        isStriped
+      >
+        <TableHeader>
+          <TableColumn>Name</TableColumn>
+          <TableColumn>{"Doctor's"}</TableColumn>
+          <TableColumn>Created At</TableColumn>
+          <TableColumn>Actions</TableColumn>
+        </TableHeader>
+        <TableBody
+          isLoading={isLoading}
+          loadingContent={<Spinner />}
+          emptyContent="No degree to display"
+        >
+          {isSuccess && degrees?.length > 0
+            ? degrees.map((degree, index) => (
+                <TableRow key={index}>
+                  <TableCell>{degree.name}</TableCell>
+                  <TableCell>0</TableCell>
+                  <TableCell>
+                    {moment(degree.createdAt).format("lll")}
+                  </TableCell>
+                  <TableCell>
+                    <></>
+                  </TableCell>
+                </TableRow>
+              ))
+            : []}
+        </TableBody>
+      </Table>
+
+      {/* add degree popup */}
+      <AddModal isOpen={isOpen} onOpenChange={onOpenChange} refetch={refetch} />
     </>
   );
 }
--- src/components/shared/VisitTimesPopup.tsx
+++ src/components/shared/VisitTimesPopup.tsx
@@ -1,138 +1,70 @@
 "use client";
 
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React from "react";
+import { IoMdClose } from "react-icons/io";
+import { GrSchedule } from "react-icons/gr";
+import { LuAlarmClock } from "react-icons/lu";
+import { Doctor } from "@prisma/client";
 
 type VisitTimesPopupProps = {
-  buttonText: React.ReactNode;
-  buttonClass: string | null;
-  style?: React.CSSProperties;
-  doctor: null | any;
+  doctor: Doctor;
 };
 
-export default function VisitTimesPopup({
-  buttonText,
-  buttonClass,
-  style,
-}: VisitTimesPopupProps) {
-  // trigger
-  const trigger = () => {
-    const body = document.querySelector("html body");
-    const popupBox = body.querySelector("div.visitTimesPopup");
-    if (popupBox) {
-    }
-  };
-
-  // add popup box in the dom
-  useEffect(() => {
-    const body = document.querySelector("html body");
-    const popupBox = body.querySelector("div.visitTimesPopup");
-    if (!popupBox) {
-      const popup = document.createElement("div");
-      popup.classList.add("visitTimesPopup");
-      body.append(popup);
-    }
-  }, []);
+export default function VisitTimesPopup({ doctor }: VisitTimesPopupProps) {
+  // modal controller
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
     <>
-      <button onClick={trigger} className={buttonClass} style={style}>
-        {buttonText}
-      </button>
+      <Button
+        onPress={onOpen}
+        className="w-1/2 !py-3 !rounded-none"
+        color="primary"
+        startContent={<GrSchedule className="w-4 h-4" />}
+      >
+        Visit Times
+      </Button>
+
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        hideCloseButton
+        backdrop="blur"
+      >
+        <ModalContent>
+          <ModalHeader>Doctor Visit Times</ModalHeader>
+          <ModalBody>Hello World</ModalBody>
+          <ModalFooter>
+            <Link href={`/appointment/book/${doctor.id}`}>
+              <Button
+                color="primary"
+                radius="sm"
+                startContent={<LuAlarmClock className="w-4 h-4" />}
+              >
+                Book Appointment
+              </Button>
+            </Link>
+            <Button
+              color="danger"
+              radius="sm"
+              startContent={<IoMdClose className="w-4 h-4" />}
+              onPress={onOpenChange}
+            >
+              Dismiss
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
-
-// const VisitTimesPopup = ({ open, close, doctor }) => {
-//   const ref = useRef();
-
-//   const handleClose = () => {
-//     close();
-//   };
-
-//   useEffect(() => {
-//     // control scrollbar
-//     const body = document.querySelector("body");
-//     if (open) {
-//       body.style.overflowY = "hidden";
-//     } else {
-//       body.style.overflowY = "auto";
-//     }
-
-//     // outside detect
-//     const __outside_click = (e) => {
-//       if (!e.target.closest(".modal_body")) {
-//         const style = window.getComputedStyle(ref.current);
-//         let opacity = parseInt(style.opacity);
-//         if (opacity === 1) {
-//           handleClose();
-//         }
-//       }
-//     };
-
-//     document.addEventListener("click", __outside_click);
-
-//     return () => {
-//       body.style.overflowY = "auto";
-//       document.removeEventListener("click", __outside_click);
-//     };
-//   }, [open]);
-
-//   return (
-//     <div
-//       ref={ref}
-//       className={`fixed top-0 left-0 w-full h-screen bg-black p-5 bg-opacity-20 grid place-items-center duration-200 transition-opacity ${
-//         open ? "visible opacity-100" : "invisible opacity-0"
-//       }`}
-//     >
-//       <div
-//         className={`modal_body bg-white p-5 rounded-xl shadow-xl sm:w-[400px] max-sm:w-full duration-200 ${
-//           open ? "opacity-100 scale-100" : "opacity-0 scale-90"
-//         }`}
-//       >
-//         <div className="flex items-center justify-between border-b pb-3 border-primary border-opacity-40">
-//           <h3 className="font-semibold text-primary">Visit Times</h3>
-//           <button
-//             onClick={handleClose}
-//             className="w-9 aspect-square rounded-full grid place-items-center bg-primary bg-opacity-10 hover:bg-opacity-100 text-primary hover:text-white duration-200"
-//           >
-//             <AiOutlineClose className="w-5 h-5" />
-//           </button>
-//         </div>
-//         <div className="mt-4 flex flex-col gap-3">
-//           <p className="text-sm">
-//             <span className="text-primary font-semibold">{doctor?.name}</span>{" "}
-//             at <span className="text-primary">Medi Care</span> Clinic are
-//             available to provide you with personalized care during the following
-//             office hours:
-//           </p>
-//           {Array.isArray(doctor?.visitTimes)
-//             ? doctor?.visitTimes?.map(
-//                 ({ day, time: { start, end } }, index) => (
-//                   <div className="flex flex-col gap-0" key={index}>
-//                     <h5 className="text-sm text-primary flex gap-2">
-//                       <span className="font-semibold">{day}:</span>
-//                       <span className="flex gap-2 items-center">
-//                         <span>{start}</span>
-//                         <span className="rounded-full h-[2px] w-2 bg-primary"></span>
-//                         <span>{end}</span>
-//                       </span>
-//                     </h5>
-//                   </div>
-//                 )
-//               )
-//             : null}
-
-//           <Link
-//             href={`/doctors/booking/${doctor?.id}`}
-//             className="text-sm mt-3 bg-primary text-center text-white border-2 border-primary font-semibold py-2.5 px-5 rounded-lg hover:bg-transparent hover:text-primary duration-200"
-//           >
-//             Book Appointment
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default VisitTimesPopup;
